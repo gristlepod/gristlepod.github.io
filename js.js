fb3f0b51140422001360ffa960249d5ec7eeefb9
texaskissrhinestone <hijirihara@protonmail.com>
2025-07-08 02:47:19 -0500

    .
    Signed-off-by: texaskissrhinestone <hijirihara@protonmail.com>

---
+++ js.js
@@ -0,0 +1,14 @@
+var fullPaintingPrice = "$90";
+var sketchPrice = "$10";
+
+function nav(){
+  document.write('<nav><a href=".">home</a> <a href="terms">terms</a><div class="navBorder"></div></nav>');
+}
+
+function socialLinks(){
+  document.write('<div id="links"><a href="https://texaskissrhinestone.github.io/stream/" target="_blank"><button style="background-image:url(&apos;ui/icons/twitch.png&apos;);" alt="Livestream @ TKR" title="Livestream @ TKR"></button></a><a href="https://ko-fi.com/gristlepod"><button style="background-image:url(&apos;ui/icons/kofi.png&apos;);" alt="Ko-fi store" title="Ko-fi store"></button></a><a href="https://x.com/ProspectorID5"><button style="background-image:url(&apos;ui/icons/twitter.png&apos;);" alt="Twitter" title="Twitter"></button></a></div>');
+}
+
+function footer(){
+  document.write("© 2025 Gristlepod. All rights reserved.");
+}
